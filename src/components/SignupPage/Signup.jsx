161877b27ff7f8139458b2e33723873e07f01de9
Teja <chepuri.teja@gmail.com>
2023-12-29 09:49:58 +0530

    Update <filename>

--- src/components/SignupPage/Signup.jsx
+++ src/components/SignupPage/Signup.jsx
@@ -1,177 +1,121 @@
 import React, { useState } from "react";
-import { useFormik } from "formik";
-import * as Yup from "yup";
-import axios from "axios";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
+  // Define state variables for form fields
+  const [username, setUsername] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // Get the navigate function from react-router-dom
   const navigate = useNavigate();
 
-  const [requestResponse, setRequestResponse] = useState({
-    textMessage: "",
-    alertClass: "",
-  });
+  // Function to handle signup form submission
+  const handleSignup = async () => {
+    // Create a data object with form field values
+    const data = {
+      username: username,
+      name: name,
+      email: email,
+      password: password,
+    };
 
-  const initialValues = {
-    Name: "",
-    email: "",
-    userName: "",
-    password: "",
-  };
-
-  const onSubmit = (values) => {
-    axios
-      .post("http://localhost:8000/api/signup/", values)
-      .then(
-        (response) => {
-          const token = response.data.token;
-
-          localStorage.setItem("authToken", token);
-
-          setRequestResponse({
-            textMessage: response.data.message,
-            alertClass: "alert alert-success",
-          });
-
-          navigate("/login");
+    try {
+      // Make a POST request to the signup API endpoint
+      const response = await fetch("http://localhost:8000/api/signup/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
         },
-        (error) => {
-          setRequestResponse({
-            textMessage: error.response.data.message,
-            alertClass: "alert alert-danger",
-          });
-        }
-      )
-      .catch((error) => console.log(error));
+        body: JSON.stringify(data),
+      });
+
+      // Handle the response from the server
+      const responseData = await response.json();
+
+      if (response.ok) {
+        // If signup is successful, navigate to the login page
+        console.log("Signup successful");
+        navigate("/login"); // Use the navigate function to go to the login page
+      } else {
+        // If signup fails, you can display an error message to the user
+        console.error("Signup failed:", responseData);
+        // Display an error message or handle failure as needed
+      }
+    } catch (error) {
+      console.error("Error during signup:", error);
+    }
   };
-
-  const validationSchema = Yup.object({
-    Name: Yup.string().required("Name is required"),
-    email: Yup.string()
-      .required("Email is required")
-      .email("Email must be a valid email"),
-    userName: Yup.string().required("UserName is required"),
-    password: Yup.string()
-      .required("Password is required")
-      .min(3, "Password must be at least 3 characters"),
-  });
-
-  const formik = useFormik({
-    initialValues,
-    onSubmit,
-    validationSchema,
-    validateOnMount: true,
-  });
 
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md-3"></div>
-        <div className="col-md-6">
-          <div className="wrapper">
-            <div className={requestResponse.alertClass} role="alert">
-              {requestResponse.textMessage}
-            </div>
-            <h2>Register</h2>
-            <hr />
-            <form onSubmit={formik.handleSubmit}>
-              <div className="form-group">
-                <label htmlFor="userName">UserName</label>
-                <input
-                  type="text"
-                  className={
-                    formik.touched.userName && formik.errors.userName
-                      ? "form-control is-invalid"
-                      : "form-control"
-                  }
-                  id="userName"
-                  name="userName"
-                  value={formik.values.userName}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                />
-                {formik.touched.userName && formik.errors.userName ? (
-                  <small className="text-danger">
-                    {formik.errors.userName}
-                  </small>
-                ) : null}
-              </div>
-              <div className="form-group">
-                <label htmlFor="Name">Name</label>
-                <input
-                  type="text"
-                  className={
-                    formik.touched.Name && formik.errors.Name
-                      ? "form-control is-invalid"
-                      : "form-control"
-                  }
-                  id="Name"
-                  name="Name"
-                  value={formik.values.Name}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                />
-                {formik.touched.Name && formik.errors.Name ? (
-                  <small className="text-danger">
-                    {formik.errors.Name}
-                  </small>
-                ) : null}
-              </div>
-              <div className="form-group">
-                <label htmlFor="email">Email</label>
-                <input
-                  type="text"
-                  className={
-                    formik.touched.email && formik.errors.email
-                      ? "form-control is-invalid"
-                      : "form-control"
-                  }
-                  id="email"
-                  name="email"
-                  value={formik.values.email}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                />
-                {formik.touched.email && formik.errors.email ? (
-                  <small className="text-danger">{formik.errors.email}</small>
-                ) : null}
-              </div>
-              <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <input
-                  type="password"
-                  className={
-                    formik.touched.password && formik.errors.password
-                      ? "form-control is-invalid"
-                      : "form-control"
-                  }
-                  id="password"
-                  name="password"
-                  value={formik.values.password}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                />
-                {formik.touched.password && formik.errors.password ? (
-                  <small className="text-danger">
-                    {formik.errors.password}
-                  </small>
-                ) : null}
-              </div>
-
-              <input
-                type="submit"
-                value="Register"
-                className="btn btn-primary btn-block"
-              />
-            </form>
-            <br />
-            <p className="text-center">
-              Already Registered <Link to="/login">Click Here</Link>
-            </p>
-          </div>
+    <div className="container d-flex justify-content-center vh-100">
+      <form className="p-4 border shadow w-100">
+        {/* Username field */}
+        <div className="mb-3">
+          <label htmlFor="username" className="form-label">
+            Username:
+          </label>
+          <input
+            type="text"
+            id="username"
+            className="form-control"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </div>
-        <div className="col-md-3"></div>
-      </div>
+
+        {/* Name field */}
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">
+            Name:
+          </label>
+          <input
+            type="text"
+            id="name"
+            className="form-control"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+
+        {/* Email field */}
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email:
+          </label>
+          <input
+            type="email"
+            id="email"
+            className="form-control"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+
+        {/* Password field */}
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password:
+          </label>
+          <input
+            type="password"
+            id="password"
+            className="form-control"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        {/* Signup button */}
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={handleSignup}
+        >
+          Signup
+        </button>
+      </form>
     </div>
   );
 };
